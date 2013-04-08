daf02ae75f3ea14f65a446986a4f3e0495dff2c7
Axujen <axujen at gmail.com>
2013-04-08 14:52:04 +0000

    First commit.
    Can conenct the mpd server and fetch playlist info, then sort it out
    into albums.

---
+++ mpdrandom.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+# © Copyright 2013 axujen, <axujen at gmail.com>. All Rights Reserved.
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""This is a script to randomly select an album in the current mpd playlist."""
+
+try:
+	import mpd
+except ImportError:
+	print('You must install the python-mpd2 library. You can get it from: '\
+			'https://pypi.python.org/pypi?:action=display&name=python-mpd2')
+	raise SystemExit
+
+# Server info
+MPD_ADDRESS = "127.0.0.1"
+MPD_PORT = "6600"
+PASSWORD = False
+SERVER_ID = {"HOST":MPD_ADDRESS, "PORT":MPD_PORT}
+
+class Client(mpd.MPDClient):
+	"""Client that connects and communicates with the mpd server."""
+
+	def __init__(self, server_id, password=False):
+		mpd.MPDClient.__init__(self)
+		self.connect(**server_id)
+		self.iterate = True
+		if password:
+			self.password(password)
+
+	def getalbums(self):
+		"""Grab a list of the albums in the playlist."""
+		albums = {}
+		for song in self.playlistinfo():
+			album = song['album']
+			if not album in albums:
+				albums[album] = [song]
+			else:
+				albums[album].append(song)
+
+		self.albums = albums
+		return albums
