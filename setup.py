932bb1ecb9658c44c675308641e3fde33bf6110f
Axujen <axujen@autistici.org>
2016-11-18 15:52:19 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,32 +1,30 @@
 #!/usr/bin/env python
-import sys
 from distutils.core import setup
-sys.path.insert(0, 'lib')
-from mpdrandom import PV
+from lib import PV
 
-setup(
-    name = "mpdrandom",
-	package_dir = {'':'lib'},
-    packages = ["mpdrandom"],
-	scripts = ['mpdrandom'],
-    version = PV,
-    description = "mpd albums randomizing script",
-    author = "Axujen",
-    author_email = "axujen@gmail",
-	url = "https://github.com/axujen/mpdrandom",
-    keywords = ["mpd", "album", "random", "shuffle", "music"],
-    classifiers = [
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Development Status :: 4 - Beta",
-        ],
-    long_description = """\
+setup(name="mpdrandom",
+      packages=["lib"],
+      scripts=['mpdrandom'],
+      version=PV,
+      description="mpd albums randomizing script",
+      author="Axujen",
+      author_email="axujen (at) autistici.org",
+      url="https://github.com/axujen/mpdrandom",
+      keywords=["mpd", "album", "random", "shuffle", "music"],
+      license="License :: OSI Approved :: GNU General Public License (GPL)",
+      classifiers=[
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 3",
+          "Development Status :: 4 - Beta",
+      ],
+    long_description="""\
 mpdrandom
 ---------
 A script that adds the missing randomness to mpds albums
 
 Features
--Pick a random album from the playlist and play it
--Daemon mode: Play a random album when you reach the end of the current one.
--Shuffle: Shuffle all albums in the current playlist.
+* Pick a random album from the playlist and play it
+* Daemon mode: Play a random album when you reach the end of the current one.
+* Shuffle: Shuffle all albums in the current playlist.
+* *NEW* Play albums randomly from the library
 """,)
